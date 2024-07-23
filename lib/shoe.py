59a02ed18ee63f18bda6f26d9f5932e2dd7fce59
Abogeerick <abogeerick@gmail.com>
2024-07-23 12:23:47 +0300

    Complete assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand, size):
+    def __init__ (self, brand, size):
         self.brand = brand
         self._size = size
-        self._condition = "New"
 
-    def set_size(self, size):
-        if isinstance (size, int):
-            self._size = size
-        else:
-           print("size must be an integer")
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
     
     def get_size(self):
         return self._size
     
-
+    def set_size(self, size):
+        if type(size) == int:
+            return size
+        else:
+            print("size must be an integer")
+        self._size = size
     
-    size = property(get_size, set_size)
-
-    def cobble (self):
-        print("Your shoe is as good as new!")
-        self._condition = "New"
-
-    def get_cobble(self):
-        return self._condition
-    
-    condition = property(get_cobble, cobble)
+    size = property(get_size,set_size)
